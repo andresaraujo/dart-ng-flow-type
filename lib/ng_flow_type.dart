4ef17f629e45872c0686194b91edbefaa6143245
Andres Araujo <a.araujo.azua@gmail.com>
2014-02-25 21:00:34 -0600

    chore(repository): Initialize repository

---
+++ lib/ng_flow_type.dart
@@ -0,0 +1,56 @@
+library angular_flow_type;
+
+import 'dart:async';
+import 'dart:html';
+import 'package:angular/angular.dart';
+
+/**
+ * Directive for changing the font-size based on a specific element's width.
+ *
+ * ## Example
+ *
+ *  <div flow-type></div>
+ */
+@NgDirective(selector: "[flow-type]",
+map: const {
+  'ft-min-font'   : '=>minFontSize',
+  'ft-max-font'   : '=>maxFontSize',
+  'ft-min-width'  : '=>minWidth',
+  'ft-max-width'  : '=>maxWidth',
+  'ft-font-ratio' : '=>fontRatio'
+})
+class FlowTypeDirective{
+  final Scope   _scope;
+  final Element _element;
+  final Window  _window;
+  
+  num minFontSize = 0;
+  num maxFontSize = 9999;
+  num maxWidth    = 9999;
+  num minWidth    = 1;
+  num fontRatio   = 35;
+    
+  FlowTypeDirective(this._element, this._scope, this._window){
+    _window.onResize.listen((_) => resize());
+    scheduleMicrotask(() {
+        resize();
+    });
+  }
+  
+  resize(){
+    var elw   = _element.offsetWidth;
+    var width = elw > maxWidth ? maxWidth : elw < minWidth ? minWidth : elw;
+    
+    var fontBase = width / fontRatio;
+    var fontSize = fontBase > maxFontSize ? maxFontSize : fontBase < minFontSize ? minFontSize : fontBase;
+    
+    _element.style.fontSize = "${fontSize}px";
+  }
+}
+
+/// Module which installs the flow-type directive
+class FlowTypeModule extends Module {
+  FlowTypeModule() {
+    type(FlowTypeDirective);
+  }
+}
---
+++ web/index.dart
@@ -0,0 +1,27 @@
+
+import 'dart:async';
+import 'package:angular/angular.dart';
+import 'package:logging/logging.dart';
+//import 'package:perf_api/perf_api.dart';
+
+import "package:angular_flow_type/ng_flow_type.dart";
+
+@MirrorsUsed(
+    targets: const [
+    ],
+    override: '*'
+)
+import 'dart:mirrors';
+
+
+class MyAppModule extends AngularModule {
+  MyAppModule(){
+    install(new FlowTypeModule());
+  }
+}
+
+main(){  
+  Logger.root.level = Level.FINEST;
+  Logger.root.onRecord.listen((LogRecord r) => print(r.message));
+  ngBootstrap(module: new MyAppModule());
+}
